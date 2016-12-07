62923150253e438de05afa7f596ed41a996bf60e
Hack Reactor Students <accounts@hackreactor.com>
2016-12-07 15:13:48 -0800

    First attempt merging

--- server/server.js
+++ server/server.js
@@ -4,23 +4,26 @@ var bodyParser = require('body-parser');
 var User = require('./users/userModel.js').User;
 
 var app = express();
-
-
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 mongoose.connect('mongodb://localhost/catFight');
 
-app.use(bodyParser.json());
+
+// var newUser = new User({ username: 'V-Cat', password: 'meow', imageUrl: 'https://s-media-cache-ak0.pinimg.com/originals/e0/81/6b/e0816b09a99f6ce0ee708343cfc5469d.png' });
 
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// newUser.save(function(){
+//   console.log('saved');
+// });
 
-app.use(express.static(__dirname + '../src/client'));
 
-var newUser = new User({ username: 'V-Cat', password: 'meow', imageUrl: 'https://s-media-cache-ak0.pinimg.com/originals/e0/81/6b/e0816b09a99f6ce0ee708343cfc5469d.png' });
 
-newUser.save(function(){
-  console.log('saved');
+app.use('/', express.static(__dirname + '../src/client'));
+
+app.post('/signup', function() {
+  console.log('do something');
 });
+
 
 app.listen(3000,function(){
   console.log('hello world');
--- server/users/userModel.js
+++ server/users/userModel.js
@@ -2,9 +2,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-  username:  {type:String, unique: true},
+  username:  {type: String, unique: true},
   password:  String,
   imageUrl:  String,
+  level: {type: Number, default: 1}
 });
 
 module.exports.User = mongoose.model('User', userSchema);
