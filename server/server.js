305caab510a4c963c8ff66fc5871efb1a2941e06
Hack Reactor Students <accounts@hackreactor.com>
2016-12-07 17:07:22 -0800

    Created Middleware

--- server/server.js
+++ server/server.js
@@ -2,12 +2,15 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var User = require('./users/userModel.js').User;
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 
 var app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 mongoose.connect('mongodb://localhost/catFight');
 
+app.use(cookieParser());
+app.use(session({secret: '1234567890QWERTY'}));
 
 // var newUser = new User({ username: 'V-Cat', password: 'meow', imageUrl: 'https://s-media-cache-ak0.pinimg.com/originals/e0/81/6b/e0816b09a99f6ce0ee708343cfc5469d.png' });
 
@@ -15,16 +18,27 @@ mongoose.connect('mongodb://localhost/catFight');
 // newUser.save(function(){
 //   console.log('saved');
 // });
+require('./config/middleware.js')(app,express);
 
+app.get('/logout', function (req, res) {
+  req.session.destroy();
+  res.redirect('../login');
+  res.send("logout success!");
+});
+app.get('/', function(req, res) {
+  console.log('Cookies: ', req.cookies)
+})
 
-
-app.use('/', express.static(__dirname + '../src/client'));
-
-app.post('/signup', function() {
+app.post('/signup', function(req,res) {
   console.log('do something');
 });
+app.get('/*', function(req, res){
+  res.redirect('/');
+})
 
 
 app.listen(3000,function(){
   console.log('hello world');
 });
+
+module.exports = app;
