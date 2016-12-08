0d6cd4b7d2be79678edd9d30c6311feb3f855ea5
Hack Reactor Students <accounts@hackreactor.com>
2016-12-07 17:09:48 -0800

    Corrected path in middleware

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -7,6 +7,6 @@ module.exports = function(app, express){
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(__dirname + '/../src/client'));
+app.use(express.static(__dirname + '../../src/client'));
 };
 
