bc4740a73efd7b4c6a73aaffa55ab89f2ce2dbc5
Hack Reactor Students <accounts@hackreactor.com>
2016-12-08 19:04:07 -0800

    Implement client side routing for login - Need to route properly when user is logging in WIP

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import {render} from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, hashHistory, browserHistory } from 'react-router';
 import {Login} from './LoginSignin.jsx';
 import {Arena} from './Arena.jsx';
 
 render((
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path="/" component={Login}/>
     <Route path="/Arena" component={Arena}/>
   </Router>
