315410bed59f0581ad1d5bc371420e2ac89ec95e
Rob0h <robert.is.oh@gmail.com>
2016-12-12 00:13:43 -0800

    Create separate components for both navigation bars and retain functionality

---
+++ src/client/app/Topbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+var Topbar = (props) => (
+  <nav className="navbar navbar-inverse navbar-fixed-top">
+    <div className="container-fluid">
+      <div className="navbar-header">
+        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span className="sr-only">Toggle navigation</span>
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+        </button>
+        <a className="navbar-brand" href="#">Cat Fight</a>
+      </div>
+      <div id="navbar" className="navbar-collapse collapse">
+        <ul className="nav navbar-nav navbar-right">
+          <li><a onClick={props.signOut}>Sign Out</a></li>
+        </ul>
+        <form className="navbar-form navbar-right">
+          <input type="text" className="form-control" placeholder="Search..."></input>
+        </form>
+      </div>
+    </div>
+  </nav>
+);
+
+export {Topbar};
