1a6eab4dbab8da7432c8f5e744afe871e9186492
Jennifer Lê <jnnnle.sf@gmail.com>
2016-12-13 16:49:51 -0800

    Will delete bundle files afterwards

---
+++ server/users/userRouter.js
@@ -0,0 +1,13 @@
+var userRouter = require('express').Router();
+var userController = require('./userController.js');
+
+userRouter.post('/login', userController.loginUser);
+userRouter.post('/signup', userController.createUser);
+userRouter.get('/logout', userController.logoutUser);
+userRouter.get('/api', userController.retrieveAll);
+userRouter.get('/:username', userController.retrieveUser);
+userRouter.put('/:username', userController.updateUser);
+userRouter.delete('/:username', userController.deleteUser);
+
+
+module.exports = userRouter;
