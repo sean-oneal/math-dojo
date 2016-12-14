f133b0a2363fe63e8ed8e292ba8f31c118a2680b
Jennifer Lê <jnnnle.sf@gmail.com>
2016-12-13 19:57:59 -0800

    Extract generate question to server

--- server/mathApi/mathController.js
+++ server/mathApi/mathController.js
@@ -14,73 +14,30 @@ module.exports = {
 
     const answer = eval(firstDigit + operands[operandIndex] + secondDigit);
 
+    const question = firstDigit + ' ' + operands[operandIndex] + ' ' + secondDigit + ' = ';
 
-    //   generateQuestion() {
+    const operand;
 
-//     const userlvl = this.props.userlvl;
-//     const operands = ['+', '-', '*', '/'];
-//     const firstDigit = Math.floor(Math.random() * Math.pow(10, userlvl));
-//     const secondDigit = Math.floor(Math.random() * Math.pow(10, userlvl));
-//     const operandIndex = Math.floor(Math.random() * 2);
+    if (operands[operandIndex] === '+') {
+      operand = 'addition';
+    } else if (operands[operandIndex] === '-') {
+      operand = 'subtraction';
+    } else if (operands[operandIndex] === '*') {
+      operand = 'multiplcation';
+    } else if (operands[operandIndex] === '/') {
+      operand = 'division';
+    }
 
-//     const answer = eval(firstDigit + operands[operandIndex] + secondDigit);
+    //send back anwser, quest, oprand (respectively)
+    res.send({
+      answer: answer, 
+      question: question,
+      operand: operand 
+    });
+  }
 
-//     this.setState({
-//       answer: answer
-//     });
-
-//     this.setState({
-//       question: firstDigit + ' ' + operands[operandIndex] + ' ' + secondDigit + ' = '
-//     });
-
-//     const operand;
-
-//     if (operands[operandIndex] === '+') {
-//       operand = 'addition';
-//     } else if (operands[operandIndex] === '-') {
-//       operand = 'subtraction';
-//     } else if (operands[operandIndex] === '*') {
-//       operand = 'multiplcation';
-//     } else if (operands[operandIndex] === '/') {
-//       operand = 'division';
-//     }
-
-//     this.setState({
-//       operand: operand
-//     });
-//   }
-
-//   checkAnswer(answer) {
-//     const context = this;
-//     if (answer === '' + this.state.answer) {
-//       const newCorrect = Object.assign({}, context.props.correctAnswers);
-//       newCorrect[context.state.operand]++;
-//       context.props.dispatch(setCorrect(newCorrect));
-
-//       this.attack();
-//       const user = document.getElementById('userContainer');
-//       // user.style.animation = 'attack2 1000ms paused';
-
-//       this.checkHealth();
-//       this.generateQuestion();
-//       //this.resetTimer();
-
-//     } else {
-//       const newIncorrect = Object.assign({}, context.props.incorrectAnswers);
-//       newIncorrect[context.state.operand]++;
-//       context.props.dispatch(setIncorrect(newIncorrect));
-
-//       this.miss();
-//       const opponent = document.getElementById('opponentContainer');
-//       // opponent.style.animation = 'attack 1000ms paused';
-
-//       this.checkHealth();
-//       this.generateQuestion();
-//       //this.resetTimer();
-//     }
-//     document.getElementById('answerForm')
-// }
-
+  checkAnswer: (req, res) => {
+    //to be implemented
   }
 }
 
