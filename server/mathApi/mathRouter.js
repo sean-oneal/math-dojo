4a9eb849922d9bae38bfb7e70c8d466ee980da5f
Jennifer Lê <jnnnle.sf@gmail.com>
2016-12-15 15:12:08 -0800

    Delete public, reset timer per problem + generate problem using userLvl

--- server/mathApi/mathRouter.js
+++ server/mathApi/mathRouter.js
@@ -2,6 +2,5 @@ const mathRouter = require('express').Router();
 const mathController = require('./mathController.js');
 
 mathRouter.get('/arena', mathController.generateQuestion);
-// // userRouter.get('/checkAnswer', mathController.checkAnswer);
 
 module.exports = mathRouter;
