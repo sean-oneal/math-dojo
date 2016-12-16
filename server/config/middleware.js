617e20c6fc8d357e12428b94f539a352f9e7f72d
sean-oneal <smoneal@gmail.com>
2016-12-15 23:33:54 -0800

    Add in Server Routes and Passport OAuth Google Authentication

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,18 +1,19 @@
-var morgan = require('morgan');
-var bodyParser = require('body-parser');
-var teacherRouter = require('./teacherRouter.js');
-var studentRouter = require('./studentRouter.js');
-var classroomRouter = require('./classroomRouter.js');
-var mathRouter = require('../mathApi/mathRouter.js'); // Added router for mathApi, route for mathRouter
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const teacherRouter = require('./teacherRouter.js');
+const studentRouter = require('./studentRouter.js');
+const classroomRouter = require('./classroomRouter.js');
+const mathRouter = require('../mathApi/mathRouter.js'); // Added router for mathApi, route for mathRouter
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-}
+
+const allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+};
 
 module.exports = function(app, express) {
   app.use(morgan('dev'));
