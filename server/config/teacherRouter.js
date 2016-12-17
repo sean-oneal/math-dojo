a447a629f508023503d587552397c2c88ccc9886
sean-oneal <smoneal@gmail.com>
2016-12-16 19:36:53 -0800

    Teacher authentication sessions persist

--- server/config/teacherRouter.js
+++ server/config/teacherRouter.js
@@ -1,7 +1,8 @@
 var teacherRouter = require('express').Router();
 var teacherController = require('./../controllers/teacherController.js');
 
-teacherRouter.post('/login', teacherController.loginTeacher);
+teacherRouter.get('/login', teacherController.loginTeacher );
+// teacherRouter.post('/login', teacherController.loginTeacher);
 teacherRouter.post('/signup', teacherController.createTeacher);
 teacherRouter.post('/logout', teacherController.logoutTeacher);
 teacherRouter.post('/student', teacherController.addStudent);
