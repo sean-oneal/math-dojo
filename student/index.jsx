0cad991fd5af0f8c7f8adde47a03ab8d274ec6b1
Aaron Trank <aarontrank@gmail.com>
2016-12-17 15:41:17 -0800

    Fixed server and client dependencies

--- student/index.jsx
+++ student/index.jsx
@@ -4,11 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import user from './reducers/index.jsx';
 import { Router, Route, hashHistory, browserHistory } from 'react-router';
-import { Login } from './teacher/Login.jsx';
 import { StudentLogin } from './student/StudentLogin.jsx';
-import { TeacherSignup } from './teacher/Signup.jsx';
-import { TeacherDashboard } from './teacher/TeacherDashboard.jsx';
-import { StudentProfile } from './teacher/StudentProfile.jsx';
 import { Arena } from './student/Arena.jsx';
 import { Chart } from './student/Chart.jsx';
 
@@ -21,11 +17,7 @@ class Root extends React.Component {
     return (
       <Provider store={store}>
         <Router history={browserHistory}>
-          <Route path="/" component={StudentLogin}/>
-          <Route path="/teacherlogin" component={Login}/>
-          <Route path="teachersignup" component={TeacherSignup}/>
-          <Route path="teacherdashboard" component={TeacherDashboard}/>
-          <Route path="studentprofile" component={StudentProfile}/>
+          <Route path="/student" component={StudentLogin}/>
           <Route path="arena" component={Arena}/>
           <Route path="chart" component={Chart}/>
         </Router>
